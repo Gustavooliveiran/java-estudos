45ff72e5559d064d27cac4458ed9e7707360a98c
Gustavo de Oliveira Nascimento <gustavo.guon26@gmail.com>
2026-05-26 23:44:58 -0300

    Create Nome:  Calculadora.java

---
+++ Nome:  Calculadora.java
@@ -0,0 +1,13 @@
+public class Calculadora {
+
+    public static void main(String[] args) {
+
+        int num1 = 10;
+        int num2 = 5;
+
+        System.out.println("Soma: " + (num1 + num2));
+        System.out.println("Subtração: " + (num1 - num2));
+        System.out.println("Multiplicação: " + (num1 * num2));
+        System.out.println("Divisão: " + (num1 / num2));
+    }
+}
